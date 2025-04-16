fab7d960f7750853d507722a896948098883856b
pa1Gowd <narganapavan5@gmail.com>
2025-04-16 23:23:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0ddc798b3f1a5117e"
+count = 4
+ami = "ami-0a2370e7c0f21e179 "
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "pavan"
+vpc_security_group_ids = ["sg-07205c612ad4b0e79"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
